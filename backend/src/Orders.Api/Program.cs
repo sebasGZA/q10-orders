c37e4939b65450b0bcd9790291520f809c9f24cd
sebasGZA <sebassga34@gmail.com>
2026-09-12 17:06:23 -0500

    fix: remove swagger and implement scalar

--- backend/src/Orders.Api/Program.cs
+++ backend/src/Orders.Api/Program.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using Orders.Api.Data;
 using Orders.Api.Orders.Application.Interfaces;
 using Orders.Api.Orders.Application.Services;
@@ -12,7 +13,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<OrdersDbContext>(options =>
@@ -36,10 +36,7 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-   {
-       options.SwaggerEndpoint("/openapi/v1.json", "Orders API");
-   });
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
